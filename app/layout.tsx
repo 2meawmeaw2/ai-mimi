29898dc5b6716c3668ca44c60df3804159696896
2meawmeaw2 <tahaslco@gmail.com>
2025-08-06 08:25:11 +0100

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { cairo, outfit } from "@/lib/fonts"; // corrected path if needed
 import { ReactLenis } from "@/lib/ReactLenis";
+import { Analytics } from "@vercel/analytics/next";
 export const metadata: Metadata = {
   title: "و أخييييراا",
   description: "كورس شاااامل في ai",
@@ -13,14 +14,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <ReactLenis root>
-        <body
-          className={` relative ${cairo.variable} ${outfit.variable} font-arabic antialiased overflow-x-clip`}
-        >
-          {children}
-        </body>
-      </ReactLenis>
-    </html>
+    <>
+      <Analytics />
+      <html lang="en">
+        <ReactLenis root>
+          <body
+            className={` relative ${cairo.variable} ${outfit.variable} font-arabic antialiased overflow-x-clip`}
+          >
+            {children}
+          </body>
+        </ReactLenis>
+      </html>
+    </>
   );
 }
